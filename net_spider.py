bf3239c35e3be40b536bc93ae81727a7931f0cc7
王颂言 <wangsongyan@wangsongyandeMacBook-Pro.local>
2020-05-21 17:25:29 +0800

    爬取任务完成

--- net_spider.py
+++ net_spider.py
@@ -7,6 +7,9 @@ import pymysql
 
 baseurl = "https://jw.nju.edu.cn/ggtz/list1.htm"
 re_url = re.compile(r'<a href="(.*htm)"')
+art_title = re.compile(r'<h1 class="arti_title">(.*)</h1>')
+art_time = re.compile(r'<span class="arti_update">发布时间：(.*)</span><span class="arti_views"')
+art_contents = re.compile(r'style="font-size:18px;line-height:1.5em;">(.*)</span>')
 
 def askURL():
     headers={
@@ -31,7 +34,7 @@ def askURL():
     req2 = urllib.request.Request(dirurl,headers=headers)
     finalhtml=""
     try:
-        res2 = urllib.request.urlopen(req)
+        res2 = urllib.request.urlopen(req2)
         finalhtml = res2.read().decode("utf-8")
         #print(html)
     except urllib.error.URLError as e:
@@ -42,7 +45,29 @@ def askURL():
     return finalhtml
 
 
+def getData():
+    html = askURL()
+    soup = BeautifulSoup(html,"html.parser")
+    item = soup.find_all("div",class_="article")[0]
+    item_temp = str(item)
+    title = re.findall(art_title,item_temp)[0]
+    time = re.findall(art_time,item_temp)[0]
+    print(time,title,sep=" ")
+    contents = soup.find("div",class_="wp_articlecontent")
+    # for i in contents.children:
+    #     print(i.string)
+    content = ""
+    for i in contents.stripped_strings:
+        content = content+i
+    print(content)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    print(askURL())
+    askURL()
+    getData()
