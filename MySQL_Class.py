255ec2e479669cc2bec268fd38a8628c4d83186e
王颂言 <wangsongyan@wangsongyandeMacBook-Pro.local>
2020-05-22 08:49:50 +0800

    爬取和数据库操作已经完成

--- MySQL_Class.py
+++ MySQL_Class.py
@@ -24,44 +24,46 @@ config2={
 }
 
 create_order = (
-     "CREATE TABLE names2 ("
-        "    id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT, "
-        "    name VARCHAR(30) DEFAULT 'AAA' NOT NULL, "
-        "    info TEXT , "  
-        "    age TINYINT UNSIGNED DEFAULT '30', "
+     "CREATE TABLE test ("
+        "    id INT UNSIGNED NOT NULL AUTO_INCREMENT, "
+        "    time VARCHAR(200) , "
+        "    title VARCHAR(200) , "
+        "    info LONGTEXT , "  
+        "    url TEXT , "
+        "    label TEXT , "
         "PRIMARY KEY (id))"
 )
 
-insert_order = "INSERT INTO names2 (name, info, age) VALUES (%s, %s, %s)"
+insert_order = "INSERT INTO test (time, title, info, url) VALUES (%s, %s, %s ,%s)"
 #在这里输入插入语句，注意格式第一个括号里面是对应的栏目，后一个括号全部是%s
 
-select_order = "SELECT id, name, info, age FROM names2 ORDER BY id"
+select_order = "SELECT title, info FROM test ORDER BY id"
 #在这里输入MySQL的SELECT语句
 
 
 class sql:
-    _cnx = 0
-    _cur = 0
+    __cnx = 0
+    __cur = 0
     def __init__(self,config):
         try:
-            self._cnx = mysql.connector.connect(**config)
+            self.__cnx = mysql.connector.connect(**config)
             print("Connected")
         except:
             print("Failed to connect")
             exit(-1)
-        self._cur = self._cnx.cursor()
+        self.__cur = self.__cnx.cursor()
     def create_table(self):
         try:
-            self._cur.execute(create_order)
-            # self._cnx.commit()
+            self.__cur.execute(create_order)
+            self.__cnx.commit()
             print("CREATE complete")
         except:
             print("Failed to create the database")
             exit(-1)
     def lines_insert(self,data):
         try:
-            self._cur.executemany(insert_order,data)
-            self._cnx.commit()
+            self.__cur.executemany(insert_order,data)
+            self.__cnx.commit()
             print("Insert complete")
         except:
             print("Failed to INSERT")
@@ -70,8 +72,8 @@ class sql:
     def select_lines(self):
         try:
             output = []
-            self._cur.execute(select_order)
-            for row in self._cur.fetchall():
+            self.__cur.execute(select_order)
+            for row in self.__cur.fetchall():
                 output.append(row)
             return output
         except:
@@ -83,6 +85,7 @@ if __name__ == "__main__":
     sql1 = sql(config1)
     info = "abc"
     data = [['Geert', info, 30], ['Jan', info, 31], ['Michel', info, 32]]
+    sql1.create_table()
     sql1.lines_insert(data)
     result = sql1.select_lines()
    
